c3d6233c313b60ca40f50b16eb424020c06f41f2
handerh <husj0423@163.com>
2019-10-16 14:12:18 +0800

    前后端的数据采用json格式进行交互

--- src/main/java/com/hsj/controller/UserController.java
+++ src/main/java/com/hsj/controller/UserController.java
@@ -5,8 +5,8 @@ import com.hsj.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 import java.util.Map;
@@ -16,7 +16,6 @@ public class UserController {
 
     @Autowired
     UserService userService;
-
     @RequestMapping("/emps")
     public String getEmployees(ModelMap map){
 
@@ -27,21 +26,62 @@ public class UserController {
         return "list";
     }
 
+
+   /* @RequestMapping("/emps")
+    public List<Employee> getEmployees(){
+
+        List<Employee> employees =  userService.getEmployees();
+        System.out.println(employees.size());
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("list");
+        return employees;
+    }*/
+
+
     @RequestMapping("/toAdd")
     public String toAddPage(){
         return "add";
     }
 
-    @RequestMapping("/addEmp")
+  /*  @RequestMapping("/addEmp")
     public String addEmp(Employee employee){
 
         System.out.println(employee);
 
         userService.addEmp(employee);
         return "redirect:/emps";
+    }*/
+
+    /*@RequestMapping("/addEmp")
+    public String addEmp(Employee employee){
+
+        System.out.println(employee);
+
+        userService.addEmp(employee);
+        return "redirect:/emps";
+    }*/
+    @ResponseBody
+    @RequestMapping("/addEmp")
+    public String addEmp(Employee employee){
+
+        System.out.println(employee);
+
+        userService.addEmp(employee);
+        return "success";
     }
 
-    @RequestMapping("/toEdit")
+    /*@RequestMapping("/toEdit")
+    public String toEditPage(Integer id,ModelMap map){
+
+        System.out.println("id: "+id);
+        Employee employee = userService.getEmployeeById(id);
+        map.put("emp",employee);
+        System.out.println(employee);
+        return "edit";
+    }*/
+    @GetMapping("/toEdit")
     public String toEditPage(Integer id,ModelMap map){
 
         System.out.println("id: "+id);
@@ -51,20 +91,39 @@ public class UserController {
         return "edit";
     }
 
-    @RequestMapping("/updateEmp")
+    /*@RequestMapping("/updateEmp")
     public String updateEmp(Employee employee){
         System.out.println(employee);
 
         userService.updateEmp(employee);
 
         return "redirect:/emps";
+    }*/
+
+    @ResponseBody
+    @RequestMapping("/updateEmp")
+    public String updateEmp(Employee employee){
+        System.out.println(employee);
+
+        userService.updateEmp(employee);
+
+        return "success";
     }
 
-    @RequestMapping("/deleteEmp")
+   /* @RequestMapping("/deleteEmp")
     public String deleteEmpById(Integer id){
 
         userService.deleteEmpById(id);
 
         return "redirect:/emps";
+    }*/
+
+    @RequestMapping("/deleteEmp")
+    @ResponseBody
+    public String deleteEmpById(Integer id){
+
+        userService.deleteEmpById(id);
+
+        return "success";
     }
 }
--- src/main/java/com/hsj/mapper/UserMapper.java
+++ src/main/java/com/hsj/mapper/UserMapper.java
@@ -1,6 +1,10 @@
 package com.hsj.mapper;
 
 import com.hsj.entity.Employee;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -8,13 +12,17 @@ import java.util.List;
 @Repository
 public interface UserMapper {
 
+    @Select("select id,name,gender,email,birth from employee")
     List<Employee> getEmployees();
 
+    @Select("select id,name,gender,email,birth from employee where id = #{id}")
     Employee getEmployeeById(Integer id);
 
+    @Insert("insert into employee(name,gender,email,birth) values(#{name},#{gender},#{email},#{birth})")
     void addEmp(Employee employee);
 
+    @Update("update employee set name=#{name},gender=#{gender},email=#{email},birth=#{birth} where id=#{id}")
     void updateEmp(Employee employee);
-
+    @Delete("delete from employee where id = #{id}")
     void deleteEmpById(Integer id);
 }
